1c753d295a256b3a9a8264e5a2817631c0f6cead
Soumalya Mondal <soumalya.mondal@capgemini.com>
2026-01-04 12:58:41 +0530

    Adding aag_range_print Script

---
+++ aag_range_print/src/main.rs
@@ -0,0 +1,8 @@
+// 7. Write a Rust program that counts from 1 to 10 using a loop and prints each number.
+
+fn main() {
+	// Count from 1 to 10 using an inclusive range and print each number
+	for number in 1..=10 {
+        println!("{number}");
+    }
+}
